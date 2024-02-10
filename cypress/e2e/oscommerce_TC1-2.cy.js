cfc0d9fab86ba6e836d5c5e6b2be10dacef949b4
Onco <adrianonco@users.noreply.github.com>
2024-02-10 11:22:33 +0100

    Added "products" fixture and the order to click in several products

--- cypress/e2e/oscommerce_TC1-2.cy.js
+++ cypress/e2e/oscommerce_TC1-2.cy.js
@@ -1,6 +1,23 @@
 /// <reference types="Cypress" />
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://demo.oscommerce.com/watch/')
-  })
-})
+
+// Define a test suite for purchasing products
+describe('OSCommerce Product Purchase Tests', () => {
+
+  // Define a test case that covers purchasing different products with specific quantities
+  it('TC1 and TC2 - Purchase different products with specific quantities', () => {
+    
+    //?
+    cy.fixture('products').then((data) => {
+
+      data.products.forEach((product) =>  {
+
+        // Step 1: Go to the specific website calling the custom command with the name of the fixture file
+        cy.navigateToUrl('urls');
+
+        // Step 2: Click the product with the given name
+        cy.contains(product.name).click();
+
+      });
+    });
+  });
+});
