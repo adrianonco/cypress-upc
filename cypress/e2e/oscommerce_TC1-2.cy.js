49947419e9c3770b660cc90aefbfc7c049dbff93
Onco <adrianonco@users.noreply.github.com>
2024-02-11 13:50:26 +0100

    Added debugging configuration

--- cypress/e2e/oscommerce_TC1-2.cy.js
+++ cypress/e2e/oscommerce_TC1-2.cy.js
@@ -2,35 +2,16 @@
 
 // Define a test suite for purchasing products
 describe('OSCommerce Product Purchase Tests', () => {
-  // Define a test case that covers purchasing different products with specific quantities
-  it('TC1 and TC2 - Purchase different products with specific quantities', () => {
-    // Load the product data from the 'products' fixture
-    cy.fixture('products').then((data) => {
-      // Iterate over each product in the loaded fixture data
-      data.products.forEach((product) =>  {
+  /* ==== Test Created with Cypress Studio ==== */
+it('Test1', function() {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit('https://sqademosatp.net/watch/');
+  cy.get('[data-id="27"] > .type-1 > .name > a').click();
+  cy.get('#btn-cart > .add-to-cart').click();
+  cy.wait(6000);
+  cy.get('.qty > .qty-box > .bigger').click();
+  cy.get('.right-buttons > .btn-2').click();
+  /* ==== End Cypress Studio ==== */
+});
 
-        // Step 1: Go to the specific website calling the custom command with the name of the fixture file
-        cy.navigateToUrl('urls');
-
-        // Step 2: Find a clickable element that contains a given name in 'products' fixture
-        cy.contains(product.name).click();
-
-        // Step 3: Click the "Add to Basket" button and wait for the pop-up form to appear
-        // As after clicking the button another page loads faster than the pop-up, the default behavior should be avoided
-        // Locate the button and operate on the jQuery object that it represents
-        cy.get('.btn-2.add-to-cart').then($button => {
-          // Attach an event handler 'e' to the 'click' event in order to intercept it
-          $button.on('click', e => {
-            // Call 'preventDefault()' to prevent the object 'e' and stop the browser navigating a new page
-            e.preventDefault(); 
-          });
-        // Trigger the click event to follow our custom logic
-        }).click();
-        // Click the 'Add to Basket' button that is shown in another pop-up
-        cy.get('.btn-2.add-to-cart').click();
-        // Wait for the pop-up form to appear as it has delay
-        cy.wait(6000);
-      });
-    });
-  });
 });
