8d697a26c0b4106126efc6f6fc11fab1e358b0c5
Onco <adrianonco@users.noreply.github.com>
2024-02-11 20:04:15 +0100

    Added timeout

--- cypress/e2e/oscommerce_TC1-2.cy.js
+++ cypress/e2e/oscommerce_TC1-2.cy.js
@@ -2,16 +2,26 @@
 
 // Define a test suite for purchasing products
 describe('OSCommerce Product Purchase Tests', () => {
-  /* ==== Test Created with Cypress Studio ==== */
-it('Test1', function() {
-  /* ==== Generated with Cypress Studio ==== */
-  cy.visit('https://sqademosatp.net/watch/');
-  cy.get('[data-id="27"] > .type-1 > .name > a').click();
-  cy.get('#btn-cart > .add-to-cart').click();
-  cy.wait(6000);
-  cy.get('.qty > .qty-box > .bigger').click();
-  cy.get('.right-buttons > .btn-2').click();
-  /* ==== End Cypress Studio ==== */
-});
+  // Define a test case that covers purchasing different products with specific quantities
+  it('TC1 and TC2 - Purchase different products with specific quantities', () => {
+    // Load the product data from the 'products' fixture
+    cy.fixture('products').then((data) => {
+      // Iterate over each product in the loaded fixture data
+      data.products.forEach((product) =>  {
 
-});
+        // Step 1: Go to the specific website calling the custom command with the name of the fixture file
+        cy.navigateToUrl('urls');
+
+        // Step 2: Find a clickable element that contains a given name in 'products' fixture
+        cy.contains(product.name).click();
+
+        // Step 3: Click the "Add to Basket" button and wait for the pop-up form to appear
+        // Click the 'Add to Basket' button that is shown in another pop-up
+        cy.get('.btn-2.add-to-cart').click();
+        cy.get('.pop-up-content', { timeout: 10000 }).should('be.visible').and('contain', 'Item');
+        // Wait for the pop-up form to appear as it has delay
+        cy.wait(6000);
+      });
+    });
+  });
+})
